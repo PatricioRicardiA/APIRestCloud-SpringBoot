28a43eb4cf4d8cc6ce93a93e7a96dfbb4e27071e
PATRICIORICARDI <patricio.ricardi@gmail.com>
2024-04-18 21:54:21 -0600

    Se agrega configuracion de prueba para el uso de roles y autoridades

--- src/main/java/com/canardi/apiSecurityCloud/Config/WebSecurityConfig.java
+++ src/main/java/com/canardi/apiSecurityCloud/Config/WebSecurityConfig.java
@@ -1,15 +1,23 @@
 package com.canardi.apiSecurityCloud.Config;
 
+import com.canardi.apiSecurityCloud.Servicios.SecurityUserDetailsService;
+import com.canardi.apiSecurityCloud.Servicios.UsuariosService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurityConfig {
+
+    @Autowired
+    private SecurityUserDetailsService userDetailsService;
 
     /*
     @Bean
@@ -18,6 +26,13 @@ public class WebSecurityConfig {
     return new InMemoryUserDetailsManager(user);
     }
     */
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http.httpBasic().and().authorizeHttpRequests()
+                .anyRequest().hasAuthority("WRITE")
+                .and().build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
